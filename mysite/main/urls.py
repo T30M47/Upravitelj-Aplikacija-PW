e8ef800f876e986d822bb0aa74f55f37775e5fa8
T30M47 <teo.matijasic@student.uniri.hr>
2023-02-14 17:52:32 +0100

    Dodan predložak za aplikacije i bootstrap.

--- mysite/main/urls.py
+++ mysite/main/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from . import views
+from main.views import *
 
 app_name = 'main'  # here for namespacing of urls.
 
 urlpatterns = [
-    path('', views.homepage, name='homepage'),
+    path('', AplikacijaList.as_view()),
 ]
--- mysite/main/views.py
+++ mysite/main/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 from django.http import HttpResponse
+from main.models import *
+
 
 ## Create your views here.
-def homepage(request):
-    return HttpResponse('Welcome to homepage! <strong>#samoOIRI</strong>')
-    # primjetiti korištenje HTML-a
+class AplikacijaList(ListView):
+    model=Aplikacija
