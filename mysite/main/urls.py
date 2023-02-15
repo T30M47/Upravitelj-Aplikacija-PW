05bacd545ef0c52907cfba90495fdffc09475822
T30M47 <teo.matijasic@student.uniri.hr>
2023-02-15 07:57:32 +0100

    Dodani MEDIA_ROOT i MEDIA_URL u settings i urls.py

--- mysite/main/urls.py
+++ mysite/main/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
 from . import views
 from main.views import *
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 app_name = 'main'  # here for namespacing of urls.
 
 urlpatterns = [
     path('', AplikacijaList.as_view()),
 ]
+if settings.DEBUG:
+        urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
